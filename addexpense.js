acb21eb74eed79866f8b9c6a679c4f8302997ebc
Saloni <saloni.patil2220@gmail.com>
2022-09-24 01:40:19 +0530

    Expense Tracker Node Js Project task 8 FRONTEND

--- addexpense.js
+++ addexpense.js
@@ -68,3 +68,48 @@ function deleteUser(Id){
         //deleteUser(expID)
 
     }
+
+    document.getElementById('rzp-button1').onclick = async function (e) {
+        const token=localStorage.getItem('token')
+        const response  = await axios.get('http://localhost:8000/premiummembership', { headers: {"Authorization" : token} });
+        console.log(response);
+        var options =
+        {
+         "key": response.data.key_id, // Enter the Key ID generated from the Dashboard
+         "name": "SALONI MILIND PATIL",
+         "order_id": response.data.order.id, // For one time payment
+         "prefill": {
+           "name": "Test User",
+           "email": "test.user@example.com",
+           "contact": "7003442036"
+         },
+         "theme": {
+          "color": "#3399cc"
+         },
+         // This handler function will handle the success payment
+         "handler": function (response) {
+             console.log(response);
+             axios.post('http://localhost:8000/updatetransactionstatus',{
+                 order_id: options.order_id,
+                 payment_id: response.razorpay_payment_id,
+             }, { headers: {"Authorization" : token} }).then(() => {
+                 alert('You are a Premium User Now')
+             }).catch(() => {
+                 alert('Something went wrong. Try Again!!!')
+             })
+         },
+      };
+      const rzp1 = new Razorpay(options);
+      rzp1.open();
+      e.preventDefault();
+    
+      rzp1.on('payment.failed', function (response){
+      alert(response.error.code);
+      alert(response.error.description);
+      alert(response.error.source);
+      alert(response.error.step);
+      alert(response.error.reason);
+      alert(response.error.metadata.order_id);
+      alert(response.error.metadata.payment_id);
+     });
+    }
